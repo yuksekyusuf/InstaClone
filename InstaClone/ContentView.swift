aa99774b07757a84fa3107ffef37a8d94240fd65
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2022-03-23 14:52:30 +0100

    Initial Commit

---
+++ InstaClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaClone
+//
+//  Created by Ahmet Yusuf Yuksek on 3/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Ahmet Yusuf Yuksek on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
